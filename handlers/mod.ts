a502f3b649e0fd62b89a9bf3ea93d984f0e65901
Roj <ez@roj.im>
2022-08-26 17:18:37 +0300

    Finalize

--- handlers/mod.ts
+++ handlers/mod.ts
@@ -15,12 +15,11 @@
  * along with Moderent.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-import { Composer, GrammyError } from "grammy/mod.ts";
-import { Chat } from "grammy/types.ts";
-import { Context, InputError, RightError } from "$utilities";
-
+import moderation from "./moderation.ts";
+import logChat from "./log_chat.ts";
 import { Context } from "$utilities";
-import { Composer } from "grammy/mod.ts";
+import { Composer } from "grammy";
+import { Chat } from "grammy/types.ts";
 
 const composer = new Composer<Context>();
 
@@ -33,38 +32,8 @@ const group = composer.filter((
 });
 
 group.filter((ctx) => !!ctx.from && ctx.session.admins.has(ctx.from.id))
-  .errorBoundary(async ({ ctx, error }) => {
-    if (error instanceof InputError) {
-      await ctx.reply(error.message);
-    } else if (error instanceof RightError) {
-      if (ctx.callbackQuery) {
-        await ctx.answerCallbackQuery({
-          text: "You can't do this.",
-          show_alert: true,
-        });
-      } else {
-        await ctx.reply(
-          `You need the following right${
-            error.requiredRights.length == 1 ? "" : "s"
-          } to perform this action:\n` +
-            error.requiredRights.join(", "),
-        );
-      }
-    } else if (error instanceof GrammyError) {
-      if (![400].includes(error.error_code)) {
-        throw error;
-      }
-      const text = `A ${error.error_code} error occurred.`;
-      if (ctx.callbackQuery) {
-        await ctx.answerCallbackQuery({ text, show_alert: true });
-      } else {
-        await ctx.reply(text);
-      }
-    }
-  }).use(
-    restriction,
-  );
+  .use(moderation);
 
 composer.filter((ctx) =>
   !!ctx.from && ctx.session.admins.get(ctx.from.id)?.status == "creator"
-).use(creator);
+).use(logChat);
--- handlers/moderation.ts
+++ handlers/moderation.ts
@@ -19,13 +19,15 @@ import {
   Context,
   getRestrictionParameters,
   getTarget,
+  InputError,
   log,
   RestrictionParameters,
   revertAction,
+  RightError,
   withRights,
 } from "$utilities";
 
-import { Composer, InlineKeyboard } from "grammy";
+import { Composer, GrammyError, InlineKeyboard } from "grammy";
 import { Chat, User } from "grammy/types.ts";
 import { fmt, mentionUser } from "grammy_parse_mode";
 
@@ -33,7 +35,37 @@ const composer = new Composer<Context>();
 
 export default composer;
 
-const message = composer.on(["message", "callback_query"]);
+const withErrorBoundary = composer.errorBoundary(async ({ ctx, error }) => {
+  if (error instanceof InputError) {
+    await ctx.reply(error.message);
+  } else if (error instanceof RightError) {
+    if (ctx.callbackQuery) {
+      await ctx.answerCallbackQuery({
+        text: "You can't do this.",
+        show_alert: true,
+      });
+    } else {
+      await ctx.reply(
+        `You need the following right${
+          error.requiredRights.length == 1 ? "" : "s"
+        } to perform this action:\n` +
+          error.requiredRights.join(", "),
+      );
+    }
+  } else if (error instanceof GrammyError) {
+    if (![400].includes(error.error_code)) {
+      throw error;
+    }
+    const text = `A ${error.error_code} error occurred.`;
+    if (ctx.callbackQuery) {
+      await ctx.answerCallbackQuery({ text, show_alert: true });
+    } else {
+      await ctx.reply(text);
+    }
+  }
+});
+
+const message = withErrorBoundary.on(["message", "callback_query"]);
 
 const canRestrict = message.use(withRights("can_restrict_members"));
 const canRestrictAndDelete = message.use(
