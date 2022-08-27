6db27d04e78225b3f7767c120c26f0bed15121d6
Roj <ez@roj.im>
2022-08-27 16:09:55 +0300

    Use `Composer.chatType` to filter chat type

--- handlers/mod.ts
+++ handlers/mod.ts
@@ -15,28 +15,23 @@
  * along with Moderent.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-import restrictions from "./restrictions.ts";
 import logChat from "./log_chat.ts";
 import messages from "./messages.ts";
+import restrictions from "./restrictions.ts";
 import { Context } from "$utilities";
 import { Composer } from "grammy";
-import { Chat } from "grammy/types.ts";
 import { autoQuote } from "grammy_autoquote";
 
 const composer = new Composer<Context>();
 
 export default composer;
 
-const group = composer.filter((
-  ctx,
-): ctx is typeof ctx & { "chat": Chat.SupergroupChat | Chat.GroupChat } => {
-  return !!ctx.chat?.type.endsWith("group");
-});
+const group = composer.chatType(["group", "supergroup"]);
 
 group.use(autoQuote);
-group.use(restrictions);
 group.use(messages);
+group.use(restrictions);
 
-composer.filter((ctx) =>
+group.filter((ctx) =>
   !!ctx.from && ctx.session.admins.get(ctx.from.id)?.status == "creator"
 ).use(logChat);
