0d2906cba4b3652619a0a9456856847a21c8b4ac
Roj <ez@roj.im>
2022-09-06 14:44:13 +0300

    Apply handlers

--- workers/mod.ts
+++ workers/mod.ts
@@ -16,6 +16,7 @@
  */
 
 import admins from "./admins.ts";
+import locks from "./locks.ts";
 import { Context } from "$utilities";
 import { Composer } from "grammy";
 
@@ -24,3 +25,4 @@ const composer = new Composer<Context>();
 export default composer;
 
 composer.use(admins);
+composer.use(locks);
