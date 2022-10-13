06efffe864474fd7f87a08031bd38e327db3ca29
Roj <ez@roj.im>
2022-10-13 14:28:36 +0300

    use warns

--- handlers/mod.ts
+++ handlers/mod.ts
@@ -21,6 +21,7 @@ import locks from "./locks.ts";
 import logChannels from "./log_channels.ts";
 import messages from "./messages.ts";
 import restrictions from "./restrictions.ts";
+import warns from "./warns.ts"
 import { Context } from "$utilities";
 import { autoQuote } from "grammy_autoquote";
 import { Composer } from "grammy";
@@ -37,3 +38,4 @@ composer.use(locks);
 composer.use(logChannels);
 composer.use(messages);
 composer.use(restrictions);
+composer.use(warns)
